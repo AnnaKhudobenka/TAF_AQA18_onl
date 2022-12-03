6461f0b96aa9e9a631196d500e6c80f7ba39d195
AnnaKhudonenka <annaavinova@gmail.com>
2022-12-03 18:15:11 +0300

    selenium

---
+++ src/main/java/factory/SimpleDriver.java
@@ -0,0 +1,14 @@
+package factory;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SimpleDriver {
+
+    public WebDriver getDriver() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\HP\\IdeaProjects\\TestAuto\\src\\test\\resources\\chromedriver.exe");
+
+        return new ChromeDriver();
+    }
+
+}
